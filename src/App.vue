5e8fc190c6fa25d25afece0bd4e10fcf46b5819a
Thaixxx <alvesthais406@gmail.com>
2021-06-11 11:37:48 -0300

    papagaio falante

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-if="mensagem == ''">Digite algo para o papagaio falante repetir</h1>
+    <div v-else>
+      <h1  class="mensagem">Papagaio falante disse</h1>
+      <h2>-{{mensagem}}</h2>
+    </div>
+    <img src="./assets/parrot.png" >
+    <br>
+    <input type="text" placeholder="digite aqui" v-model="mensagem"><br>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return{
+      mensagem: "",
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,7 +30,29 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+}
+h1{
+  margin-top:50px;
+   color:#8B3A3A;
+}
+h2{
+  color: #CD5555;
+}
+input{
+  width:200px;
+  height:15px;
+  margin-top:15px;
+  background-color:#EEC591;
+  border-color: #CD5555;
+  border: 2.5px solid;
+  color: #CD5555;
+  text-color: red;
+  text-align :center;
+}
+
+img{
+  width:380px;
+  height:240px
 }
 </style>
